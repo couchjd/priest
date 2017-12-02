f43214b193f50f138e429f0b6bf1b68214e92d48
couchjd <couchjeffrey@yahoo.com>
2017-12-01 20:21:23 -0800

    initial commit

---
+++ priest/main.cpp
@@ -0,0 +1,23 @@
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
+#define TITLE "priest"
+
+int main() {
+  sf::RenderWindow window(sf::VideoMode(1280, 720), TITLE);
+
+  while(window.isOpen()) {
+    sf::Event event;
+    while(window.pollEvent(event)) {
+      if(event.type == sf::Event::Closed) {
+        window.close();
+      }
+    }
+
+    window.clear();
+
+    window.display();
+  }
+  return 0;
+}
