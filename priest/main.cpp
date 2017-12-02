6a5b33debf7f1446eb76199b210bbcff21a5e162
couchjd <couchjeffrey@yahoo.com>
2017-12-02 08:05:03 -0800

    added player movement

--- priest/main.cpp
+++ priest/main.cpp
@@ -17,13 +17,40 @@ int main() {
       if(event.type == sf::Event::Closed) {
         window.close();
       }
+
+      if(event.type == sf::Event::KeyPressed) {
+        if(event.key.code == sf::Keyboard::Up) {
+          player.setPosition(player.getPosition().x, player.getPosition().y - 20.f);
+        }
+        if(event.key.code == sf::Keyboard::Down) {
+          player.setPosition(player.getPosition().x, player.getPosition().y + 20.f);
+        }
+        if(event.key.code == sf::Keyboard::Left) {
+          player.setPosition(player.getPosition().x - 20.f, player.getPosition().y);
+        }
+        if(event.key.code == sf::Keyboard::Right) {
+          player.setPosition(player.getPosition().x + 20.f, player.getPosition().y);
+        }
+      }
     }
 
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
       window.close();
     }
-    
-    window.clear(sf::Color::Magenta);
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+      player.setPosition(player.getPosition().x, player.getPosition().y - 1.f);
+    }
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+      player.setPosition(player.getPosition().x, player.getPosition().y + 1.f);
+    }
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+      player.setPosition(player.getPosition().x - 1.f, player.getPosition().y);
+    }
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+      player.setPosition(player.getPosition().x + 1.f, player.getPosition().y);
+    }
+
+    window.clear(sf::Color::Cyan);
     window.draw(player);
     window.display();
   }
